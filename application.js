9c3bbb7eca0ea5e84c082acdbc968f7c4f99be53
Rodrigo Haenggi <rodrigo@codegestalt.com>
2022-02-02 14:17:01 +0100

    Part 4

--- application.js
+++ application.js
@@ -7,29 +7,36 @@ document.addEventListener("DOMContentLoaded", (_) => {
 // Game
 function startGame() {
   const randomNumber = Math.floor(Math.random() * 101);
-  logLine("Please enter a number between 0 and 100.\n")
+  logLine("Please enter a number between 0 and 100.<br>")
 
   document.addEventListener('answerSubmit', (event) => {
 
     var guess = parseInt(event.detail)
 
     console.log(randomNumber)
-    console.log(guess)
 
-    if (guess < 0) logLine("Please enter a positive number.\n");
-    if (guess > 100) logLine("Please enter a number below 100.\n");
-    if (guess < randomNumber) logLine("Your number is too low. Try again.\n");
-    if (guess > randomNumber) logLine("Your number is too high. Try again.\n");
-    if (guess === randomNumber) logLine("Yaaay, you won!\n");
+    if (guess < 0) logLine("<span class='red'>Please enter a positive number.</span><br>");
+    if (guess > 100) logLine("<span class='red'>Please enter a number below 100.</span><br>");
+    if (guess < randomNumber) logLine("<span class='orange'>Your number is too low. Try again.</span><br>");
+    if (guess > randomNumber) logLine("<span class='orange'>Your number is too high. Try again.</span><br>");
+    if (guess === randomNumber) logWin();
   })
 }
 
 // Functions
 function logLine(text) {
   var log = document.querySelector("[data-target='log'")
-  log.innerText += `${text}`
+  log.innerHTML += `${text}`
   event.preventDefault();
   return log.innerText;
+}
+
+function logWin() {
+  logLine("<span class='green'>Yaaay, you've won!</span><br>");
+  var log = document.querySelector("[data-target='log']")
+  var img = document.createElement("img");
+  img.src = "https://cataas.com/cat"
+  log.appendChild(img)
 }
 
 // Setup
@@ -38,7 +45,7 @@ function setupFormField() {
   const textField = document.querySelector("[data-target='text-field']")
 
   form.addEventListener('submit', (_) => {
-    logLine("> " + textField.value + "\n")
+    logLine("> " + textField.value + "<br>")
     const event = new CustomEvent('answerSubmit', { detail: textField.value });
     document.dispatchEvent(event);
     textField.value = ""
