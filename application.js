cce6669468ae81d874a4e16474e9908a3325e9fe
Rodrigo Haenggi <rodrigo@codegestalt.com>
2022-02-02 13:48:03 +0100

    Part 2

--- application.js
+++ application.js
@@ -1,13 +1,25 @@
 // What to do once the HTML Document Object Model (DOM) has been completely loaded
 document.addEventListener("DOMContentLoaded", (_) => {
   setupFormField();
+  startGame();
 });
+
+// Game
+function startGame() {
+  const randomNumber = Math.floor(Math.random() * 101);
+  logLine("Please enter a number between 0 and 100.\n")
+
+  document.addEventListener('answerSubmit', (event) => {
+    logLine(`You've guessed ${event.detail}, the random number was ${randomNumber}\n`)
+  })
+}
 
 // Functions
 function logLine(text) {
   var log = document.querySelector("[data-target='log'")
-  log.innerText += `${text}\n`
+  log.innerText += `${text}`
   event.preventDefault();
+  return log.innerText;
 }
 
 // Setup
@@ -16,7 +28,10 @@ function setupFormField() {
   const textField = document.querySelector("[data-target='text-field']")
 
   form.addEventListener('submit', (_) => {
-    logLine(textField.value)
+    logLine("> " + textField.value + "\n")
+    const event = new CustomEvent('answerSubmit', { detail: textField.value });
+    document.dispatchEvent(event);
     textField.value = ""
+
   });
 }
