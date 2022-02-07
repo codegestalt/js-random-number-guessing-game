a3fb1636262cc923bec1863fe969d1dd9fb0d0ac
Rodrigo Haenggi <rodrigo@codegestalt.com>
2022-02-07 16:50:23 +0100

    Part 5 - Reset Game

--- application.js
+++ application.js
@@ -1,29 +1,34 @@
-// What to do once the HTML Document Object Model (DOM) has been completely loaded
 document.addEventListener("DOMContentLoaded", (_) => {
   setupFormField();
   startGame();
 });
 
-// Game
 function startGame() {
-  const randomNumber = Math.floor(Math.random() * 101);
+  window.randomNumber = Math.floor(Math.random() * 101);
   logLine("Please enter a number between 0 and 100.<br>")
-
-  document.addEventListener('answerSubmit', (event) => {
-
-    var guess = parseInt(event.detail)
-
-    console.log(randomNumber)
-
-    if (guess < 0) logLine("<span class='red'>Please enter a positive number.</span><br>");
-    if (guess > 100) logLine("<span class='red'>Please enter a number below 100.</span><br>");
-    if (guess < randomNumber) logLine("<span class='orange'>Your number is too low. Try again.</span><br>");
-    if (guess > randomNumber) logLine("<span class='orange'>Your number is too high. Try again.</span><br>");
-    if (guess === randomNumber) logWin();
-  })
+  document.addEventListener('answerSubmit', submitAnswerHandler)
 }
 
-// Functions
+function submitAnswerHandler(event) {
+  var guess = parseInt(event.detail)
+  var randomNumber = window.randomNumber
+
+  console.log(randomNumber)
+
+  if (guess < 0) logLine("<span class='red'>Please enter a positive number.</span><br>");
+  if (guess > 100) logLine("<span class='red'>Please enter a number below 100.</span><br>");
+  if (guess < randomNumber) logLine("<span class='orange'>Your number is too low. Try again.</span><br>");
+  if (guess > randomNumber) logLine("<span class='orange'>Your number is too high. Try again.</span><br>");
+  if (guess === randomNumber) logWin();
+}
+
+function resetGame() {
+  var log = document.querySelector("[data-target='log'")
+  log.innerHTML = ""
+  document.removeEventListener("answerSubmit", submitAnswerHandler)
+  startGame();
+}
+
 function logLine(text) {
   var log = document.querySelector("[data-target='log'")
   log.innerHTML += `${text}`
@@ -39,10 +44,10 @@ function logWin() {
   log.appendChild(img)
 }
 
-// Setup
 function setupFormField() {
   const form = document.querySelector("[data-target='input-form']")
   const textField = document.querySelector("[data-target='text-field']")
+  const resetGameLink = document.querySelector("[data-target='reset-game'")
 
   form.addEventListener('submit', (_) => {
     logLine("> " + textField.value + "<br>")
@@ -50,4 +55,9 @@ function setupFormField() {
     document.dispatchEvent(event);
     textField.value = ""
   });
+
+  resetGameLink.addEventListener('click', (event) => {
+    resetGame()
+    event.preventDefault()
+  })
 }
