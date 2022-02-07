77bea270cfdd288c2d1675ac349fde10f458de5e
Rodrigo Haenggi <rodrigo@codegestalt.com>
2022-02-07 17:03:26 +0100

    Part 6 - Number of tries + lock forms

--- application.js
+++ application.js
@@ -5,6 +5,9 @@ document.addEventListener("DOMContentLoaded", (_) => {
 
 function startGame() {
   window.randomNumber = Math.floor(Math.random() * 101);
+  window.maxTries = 10
+  window.tries = 0
+  logTry(tries, maxTries)
   logLine("Please enter a number between 0 and 100.<br>")
   document.addEventListener('answerSubmit', submitAnswerHandler)
 }
@@ -20,11 +23,19 @@ function submitAnswerHandler(event) {
   if (guess < randomNumber) logLine("<span class='orange'>Your number is too low. Try again.</span><br>");
   if (guess > randomNumber) logLine("<span class='orange'>Your number is too high. Try again.</span><br>");
   if (guess === randomNumber) logWin();
+
+  window.tries += 1
+  logTry(tries, maxTries)
+  if (tries === maxTries) {
+    logLine("<span class='red'>GAME OVER</span><br>")
+    lockForm()
+  }
 }
 
 function resetGame() {
   var log = document.querySelector("[data-target='log'")
   log.innerHTML = ""
+  unlockForm()
   document.removeEventListener("answerSubmit", submitAnswerHandler)
   startGame();
 }
@@ -42,6 +53,21 @@ function logWin() {
   var img = document.createElement("img");
   img.src = "https://cataas.com/cat"
   log.appendChild(img)
+}
+
+function logTry(number, maxTries) {
+  var text = document.querySelector("[data-target='tries-text']")
+  text.innerText = `(Try ${number} of ${maxTries})`
+}
+
+function lockForm() {
+  const form = document.querySelector("[data-target='input-form']")
+  form.querySelector("button").disabled = true
+}
+
+function unlockForm() {
+  const form = document.querySelector("[data-target='input-form']")
+  form.querySelector("button").disabled = false
 }
 
 function setupFormField() {
